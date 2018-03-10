56060e9c7d6836153a621fd91100b3cb853a77d7
myhappyagency <31253241+myhappyagency@users.noreply.github.com>
2018-03-10 18:14:38 +0100

    Initing

---
+++ assets/js/scriptseb.js
@@ -0,0 +1,337 @@
+(function(window, io) {
+
+  'use strict';
+
+  window.addEventListener('DOMContentLoaded', function() {
+
+    let socketIo = io('http://localhost:3000'); // adresse io('http://192.168.1.19:3000')
+
+    let data = {
+      startButton: window.document.getElementById('play'),
+      startScreen: window.document.getElementById('chargement'),
+      beforeButton: window.document.getElementById('enter-game'),
+      beforeScreen: window.document.getElementById('selection'),
+      trump: window.document.getElementById('stormy'),
+      kimkardashian: window.document.getElementById('kimmy'),
+      joueurActuel: {
+        couleur: '',
+        life: 200,
+        score: 0
+      },
+      couleurs: {
+        y: '#f7d411',
+        r: 'tomato',
+        ro: 'deeppink',
+        lm: 'limegreen',
+        dr: 'red',
+        w: 'white'
+      },
+      kim: false,
+      gameOver: false
+    }
+
+    let start = function() {
+
+      // Créer un joueur
+      var creerUnJoueur = function(joueur) {
+        var div = window.document.createElement('div');
+        div.id = joueur.id;
+        div.style.top = joueur.x + 'px';
+        div.style.left = joueur.y + 'px';
+        div.style.width = joueur.width;
+        div.style.height = '270px'; // joueur.height
+        div.style.paddingTop = '20px'; // joueur.height
+        div.style.position = 'absolute';
+        div.style.overflow = 'hidden';
+        window.document.body.appendChild(div);
+
+        var nouveauPlayer = document.createElement('img')
+        nouveauPlayer.id = joueur.idImage;
+        nouveauPlayer.className = 'storm';
+        if (joueur.kim == false) {
+          nouveauPlayer.src = '/images/trump.png';
+        } else {
+          nouveauPlayer.src = '/images/kim2.png';
+        }
+        div.appendChild(nouveauPlayer);
+        // Envoyer au serveur si Kim est true ou pas
+
+        var barre = window.document.createElement('div');
+        var name = document.createTextNode(joueur.name);
+        barre.appendChild(name);
+        barre.style.width = joueur.width;
+        barre.style.height = 10 + 'px';
+        barre.style.top = '0px';
+        barre.style.position = 'absolute';
+        barre.style.fontSize = '14px';
+        barre.style.textAlign = 'center';
+        barre.style.color = joueur.color;
+        div.appendChild(barre);
+
+      };
+
+      // Mettre à jour sa position
+      var updateDivPosition = function(joueur) {
+        var div = window.document.getElementById(joueur.id);
+        div.style.top = joueur.y + 'px';
+        div.style.left = joueur.x + 'px';
+      };
+
+      // Supprimer s'il se déconnecte
+      var removeDiv = function(divId) {
+        var div = window.document.getElementById(divId);
+        if (div) {
+          window.setTimeout(function() {
+            div.parentNode.removeChild(div);
+          }, 500);
+        }
+      };
+
+      // Créer mon Interface Score
+      var creerMaBarreDeScore = function(donnees) {
+        var div = window.document.createElement('div');
+        div.setAttribute('id', 'barreDeScore');
+        div.style.height = donnees.height;
+        div.style.width = donnees.width;
+        div.style.top = donnees.top;
+        div.style.margin = donnees.margin;
+        div.style.position = donnees.position;
+        div.style.transition = donnees.transition;
+        div.style.color = donnees.color;
+        div.style.fontSize = donnees.fontSize;
+        document.body.appendChild(div);
+        $("#barreDeScore").html("<p>" + data.joueurActuel.score + " globes</p>")
+      }
+
+      // Créer mon Interface Score
+      var creerBarreDeScoreGeneral = function(donnees) {
+        var div = window.document.createElement('div');
+        div.setAttribute('id', 'barreDeScoreGeneral');
+        div.style.height = donnees.height;
+        div.style.width = donnees.width;
+        div.style.top = donnees.top;
+        div.style.right = donnees.right;
+        div.style.border = donnees.border;
+        div.style.margin = donnees.margin;
+        div.style.position = donnees.position;
+        div.style.transition = donnees.transition;
+        document.body.appendChild(div);
+      }
+
+      // Créer ma barre de vie
+      var creerMaBarreDeVie = function(donnees) {
+        var div = window.document.createElement('div');
+        div.setAttribute('id', 'barre');
+        div.style.height = donnees.height;
+        div.style.width = data.joueurActuel.life + 'px';
+        div.style.maxWidth = '200px';
+        div.style.margin = donnees.margin;
+        div.style.overflow = donnees.overflow;
+        div.style.transition = donnees.transition;
+        div.style.position = donnees.position;
+        div.style.backgroundColor = donnees.backgroundColor;
+        document.body.appendChild(div);
+      }
+
+      // Créer un globe de vie
+      var creerUnGlobeDeVie = function(globe) {
+        var div = window.document.createElement('img');
+        div.setAttribute('class', 'heart');
+        div.style.top = globe.top;
+        div.style.left = globe.left;
+        div.src = globe.src;
+        div.id = globe.id;
+        div.style.width = globe.width;
+        div.style.height = globe.height;
+        div.style.position = globe.position;
+        div.style.overflow = globe.overflow;
+        div.style.backgroundColor = globe.backgroundColor;
+        div.style.borderRadius = globe.borderRadius;
+        div.style.padding = globe.padding;
+        window.document.body.appendChild(div);
+
+        // Quand je clique sur le globe de vie, il disparaît, ma vie remonte et je gagne 1 point
+        $('#' + globe.id).on('click', function() {
+          data.joueurActuel.life = data.joueurActuel.life + 20;
+          data.joueurActuel.score += 1;
+          $("#barreDeScore").html("<p>" + data.joueurActuel.score + " globes</p>")
+          socketIo.emit('globeEnglouti', {id: globe.id});
+          socketIo.emit('scoreAugmente', {score: data.joueurActuel.score});
+        })
+      }
+
+      function Lave() {
+        data.joueurActuel.life = data.joueurActuel.life - 4;
+        console.log(data.joueurActuel.life);
+        var div = window.document.getElementById('barre');
+        div.style.width = data.joueurActuel.life + 'px';
+        if (data.joueurActuel.life >= 100) {
+          div.style.backgroundColor = data.couleurs.lm;
+        } else if (data.joueurActuel.life >= 50) {
+          div.style.backgroundColor = data.couleurs.y;
+        } else if (data.joueurActuel.life >= 1) {
+          div.style.backgroundColor = data.couleurs.r;
+        } else if (data.joueurActuel.life === 0) {
+          console.log('joueur mort');
+          GameOver();
+        }
+      }
+      var idLave = setInterval(Lave, 1000); // La Lave tue tout le monde
+
+      function GameOver() {
+        clearInterval(idLave);
+        socketIo.emit('mortDunJoueur');
+        data.gameOver = true;
+        $('#gameover').css('top', '0%');
+        console.log('Interval cleared.');
+      }
+
+      /*
+
+
+    Partie Socket
+
+
+    */
+
+      // Envoi des coordonnées temps réel quand joueur connecté
+      socketIo.on('myJoueurCreated', function(myJoueur) {
+        window.document.addEventListener('mousemove', function(event) {
+          if (data.gameOver == false) {
+            socketIo.emit('moveJoueur', {
+              x: event.clientX - 72,
+              y: event.clientY - 125
+            });
+          }
+        });
+      });
+
+      // Maj Position des joueurs
+      socketIo.on('updateJoueurPosition', function(joueur) {
+        updateDivPosition(joueur);
+      });
+
+      // Globes
+      socketIo.on('unNouveauGlobeApparait', function(data) {
+        creerUnGlobeDeVie(data);
+      });
+      socketIo.on('creerUnGlobe', function(data) {
+        creerUnGlobeDeVie(data);
+      });
+
+      // On clique sur un globe, il disparaît pour tous
+      socketIo.on('globeDisparait', function(data) {
+        $('#' + data.id).remove();
+      })
+
+      // Créer la barre de vie
+      socketIo.on('creerLaBarreDeVie', function(data) {
+        creerMaBarreDeVie(data);
+      })
+
+      // Créer la barre de score du perso
+      socketIo.on('creerLaBarreDeScore', function(data) {
+        creerMaBarreDeScore(data);
+      })
+
+      // Créer la barre de score générale
+      socketIo.on('creerLaBarreDeScoreGeneral', function(data) {
+        creerBarreDeScoreGeneral(data);
+      })
+
+      // Un nouveau joueur se connecte, je l'ajoute
+      socketIo.on('ajoutDunNouveauJoueur', function(joueur) {
+        console.log(joueur);
+        creerUnJoueur(joueur);
+      });
+
+      // Un joueur se déconnecte, je le supprime
+      socketIo.on('removeJoueur', function(divId) {
+        console.log('Ce joueur quitte la partie: ', divId);
+        removeDiv(divId);
+      });
+
+      // Je suis un nouveau joueur, je récupère les anciens et les rajoute
+      socketIo.on('jeRecupLaListeDesJoueurs', function(liste) {
+        var array = $.map(liste, function(value, index) {
+          return [value];
+        });
+        array.forEach(function(joueur) {
+          creerUnJoueur(joueur);
+        });
+      });
+
+      // Je crée le tableau
+      socketIo.on("creeLeTableau", function(data) {
+        console.log(data);
+        data.forEach(function(idNouveauxJoueurs) {
+          $('#barreDeScoreGeneral').append('<p>' +
+            '<span>' + idNouveauxJoueurs.nom + '</span>' + ' ' + '<span class="right" style="color:#f7d411;">' + idNouveauxJoueurs.score + '</span>' + '</p>')
+        })
+      });
+
+      // Un joueur meurt
+      socketIo.on('jeVeuxMonID', function(monFuckingId) {
+        console.log("Je suis mort :" + monFuckingId);
+        // Si un personnage meurt, il se transforme en tête de mort
+        $("#" + monFuckingId).removeClass('storm_marche').addClass('mort')
+      })
+
+      /* A FAIRE */
+
+      // Mettre à jour le tableau renvoyé par majTableau
+      socketIo.on('majTableau', function(data) {
+        // console.log("Mise à jour du tableau des scores");
+        // console.log(data);
+        $('#barreDeScoreGeneral').empty();
+        data.forEach(function(idNouveauxJoueurs) {
+          $('#barreDeScoreGeneral').append('<p>' +
+            '<span>' + idNouveauxJoueurs.nom + '</span>' + ' ' + '<span class="right" style="color:#f7d411;">' + idNouveauxJoueurs.score + '</span>' + '</p>')
+        })
+      })
+
+    };
+
+    // Interface Générale
+
+    data.startButton.addEventListener('click', function() {
+      document.body.removeChild(data.startScreen);
+    }) // 1ère page: Start button
+
+    data.trump.addEventListener('click', function() {
+      data.kim = false;
+      console.log(data.kim);
+      data.trump.style.border = '1px solid #f7d411';
+      data.kimkardashian.style.border = 'none';
+    }) // Ne pas choisir kim
+
+    data.kimkardashian.addEventListener('click', function() {
+      data.kim = true;
+      console.log(data.kim);
+      data.kimkardashian.style.border = '1px solid #f7d411';
+      data.trump.style.border = 'none';
+    }) // Choisir kim
+
+    $('#inscription').on('submit', function(event) {
+      event.preventDefault();
+      socketIo.emit('jenvoieMonNom', {
+        nom: $('#player_name').val(),
+        kim: data.kim,
+        score: data.joueurActuel.score
+      });
+      document.body.removeChild(data.beforeScreen);
+      start();
+    })
+
+    // Si le pseudo est déjà pris
+    socketIo.on('pseudoDejaPris', function() {
+      $('#inscription').on('submit', function(event) {
+        event.preventDefault();
+      });
+      console.log("ca marche");
+      $('#alerte').css('top', '0%');
+    })
+
+  }); // DOMContentLoaded
+
+}(window, io)); // IIFE
---
+++ server.js
@@ -0,0 +1,383 @@
+/* Serveur maj: 10/03/2018 à 17:56:13 */
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const MongoClient = require('mongodb').MongoClient;
+
+const monServeur = express();
+const monServeurIo = require('http').createServer(monServeur);
+const socketIO = require('socket.io')(monServeurIo);
+
+const port = process.env.PORT || 3000;
+const url = 'mongodb+srv://guillaume:86327417Gd%2E@donaldvskim-zqkaf.mongodb.net'
+// const url = 'mongodb://localhost:27017'
+const dbName = 'users'
+
+/*
+  SSH Mongo: mongo "mongodb+srv://donaldvskim-zqkaf.mongodb.net/test" --username guillaume
+
+  /Applications/mongodb/bin/mongod --config=/Users/guillaume/Desktop/Guillaume/Game/mongod.conf &
+/Applications/mongodb/bin/mongo
+*/
+
+monServeur.use('/css', express.static(__dirname + '/assets/css'))
+monServeur.use('/js', express.static(__dirname + '/assets/js'))
+monServeur.use('/images', express.static(__dirname + '/static/medias'))
+
+monServeur.get('/', function(req, res, next) {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, './static')
+  });
+})
+
+// Partie Jeu
+
+var data = {
+  barreDeVie: {
+    mw: 200 + 'px',
+    h: 15 + 'px',
+    mg: 25 + 'px',
+    o: 'hidden',
+    p: 'fixed',
+    tr: '.25s'
+  },
+  barreDeScoreDuPerso: {
+    w: 200 + 'px',
+    h: 30 + 'px',
+    t: 25 + 'px',
+    fs: 18 + "px"
+  },
+  barreDeScoreGeneral: {
+    w: 300 + 'px',
+    h: 200 + 'px',
+    r: 20 + 'px',
+    br: 'none'
+  },
+  barreDePseudo: {
+    w: 200 + 'px',
+    h: 50 + 'px',
+    p: 'relative',
+    ta: 'center',
+    fs: 12 + 'px'
+  },
+  cadre: {
+    x: 0 + 'px',
+    y: 0 + 'px',
+    h: 250 + 'px',
+    w: 125 + 'px',
+    l: 125 + 'px',
+    o: 'hidden',
+    txy: 'translate(-50%, -50%)'
+  },
+  coeur: {
+    src: '/images/hearts.png',
+    x: 0 + 'px',
+    y: 0 + 'px',
+    h: 50 + 'px',
+    w: 50 + 'px',
+    o: 'hidden',
+    t: 0 + 'px',
+    l: 0 + 'px',
+    p: 'absolute',
+    br: 50 + 'px',
+    pa: 10 + 'px'
+  },
+  couleurs: {
+    y: '#f7d411',
+    r: 'tomato',
+    ro: 'deeppink',
+    lm: 'limegreen',
+    dr: 'red',
+    w: 'white'
+  }
+}
+
+/*
+  Fonctions primaires
+  */
+
+// Générer un id
+const idGenerator = function() {
+  function idPart() {
+    return (((1 + Math.random()) * 0x1000000) | 0).toString(16).substring(1);
+  }
+  return idPart() + idPart() + idPart() + idPart();
+}
+
+// Générér une couleur
+const randomColor = function() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const Joueur = function(nom, kim, idImage) {
+  // this.choice = Renvoyer le valeur kim ou trump depuis le formulaire
+  this.name = nom;
+  this.kim = kim;
+  this.x = data.cadre.x;
+  this.y = data.cadre.y;
+  this.id = idGenerator();
+  this.idImage = idImage;
+  this.width = data.cadre.w;
+  this.height = data.cadre.h;
+  this.color = randomColor();
+  this.overflow = data.cadre.o;
+  this.updatePosition = function(clientX, clientY) {
+    this.x = clientX;
+    this.y = clientY;
+  };
+  this.getClientObject = function() {
+    return {
+      name: this.name,
+      kim: this.kim,
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height,
+      o: this.overflow,
+      id: this.id,
+      idImage: this.idImage,
+      color: this.color
+    }
+  }
+};
+
+// Créer la barre de pseudo du joueur
+const InterfaceBarrePseudo = function() {
+  this.color = randomColor();
+}
+
+// Créer l'interface joueur
+const InterfaceJoueur = function() {
+  this.height = data.barreDeVie.h;
+  this.width = data.barreDeVie.mw;
+  this.margin = data.barreDeVie.mg;
+  this.overflow = data.barreDeVie.o;
+  this.position = data.barreDeVie.p;
+  this.transition = data.barreDeVie.tr;
+  this.backgroundColor = data.couleurs.lm;
+}
+
+// Créer l'interface de score du joueur
+const InterfaceScoreJoueur = function() {
+  this.height = data.barreDeScoreDuPerso.h;
+  this.width = data.barreDeScoreDuPerso.w;
+  this.top = data.barreDeScoreDuPerso.t;
+  this.margin = data.barreDeVie.mg;
+  this.position = data.barreDeVie.p;
+  this.transition = data.barreDeVie.tr;
+  this.color = data.couleurs.y;
+  this.fontSize = data.barreDeScoreDuPerso.fs;
+}
+
+// Créer l'interface de score générale
+const ScoreGeneral = function() {
+  this.height = data.barreDeScoreGeneral.h;
+  this.width = data.barreDeScoreGeneral.w;
+  this.right = data.barreDeScoreGeneral.r;
+  this.border = data.barreDeScoreGeneral.br;
+  this.margin = data.barreDeVie.mg;
+  this.overflow = data.barreDeVie.o;
+  this.position = data.barreDeVie.p;
+  this.transition = data.barreDeVie.tr;
+}
+
+// Globes de vies
+const UsineCoeurs = function(a, b) {
+  this.top = a,
+  this.left = b,
+  this.id = idGenerator();
+  this.src = data.coeur.src,
+  this.width = data.coeur.w;
+  this.height = data.coeur.h;
+  this.position = data.coeur.p;
+  this.backgroundColor = data.couleurs.lm;
+  this.borderRadius = data.coeur.br;
+  this.padding = data.coeur.pa;
+  this.getClientObject = function() {
+    return {
+      t: this.top,
+      l: this.left,
+      id: this.id,
+      src: this.src,
+      w: this.width,
+      h: this.height,
+      p: this.position,
+      bg: this.backgroundColor,
+      br: this.borderRadius,
+      pa: this.padding
+    }
+  }
+}
+
+// Connect to mongodb
+MongoClient.connect(url, function(err, client) {
+
+  if (err) {
+    throw err
+  } else {
+    console.log('Connexion à MongoDB réussie: ' + url);
+  }
+
+  let db = client.db('users'); // Indispensable
+  let liste = db.collection('liste');
+  let scores = db.collection('scores');
+
+  const listeDesJoueurs = {};
+  let size = Object.keys(listeDesJoueurs).length;
+
+  socketIO.on('connection', function(socket) {
+
+    function globesDeVie() {
+      const packageVie = new UsineCoeurs(Math.floor(Math.random() * 800) + 20 + 'px', Math.floor(Math.random() * 1000) + 20 + 'px');
+      socket.emit('creerUnGlobe', packageVie);
+      socket.broadcast.emit('unNouveauGlobeApparait', packageVie);
+    };
+
+    // Transformer en array l'objet des joueurs en ligne retournés
+    listeDesJoueursArray = Object.keys(listeDesJoueurs).map(key => listeDesJoueurs[key]);
+
+    // J'envoie la liste de tous les joueurs
+    socket.emit('allJoueurs', listeDesJoueursArray);
+
+    // Création d'un joueur
+    socket.on('jenvoieMonNom', function(newplayer) {
+
+      // Checke si le nom existe déjà
+      liste.find({"nom": newplayer.nom}).toArray(function(err, data) {
+        // S'il existe déjà, j'empêche d'entrer
+        if (data.length > 0) {
+          socket.emit('pseudoDejaPris', data)
+          console.log('Pseudo déjà pris');
+        } else {
+          // Sinon, je lance le bordel
+          // Je me connecte à la bdd et j'insère le nouveau joueur dans la liste s'il n'existe pas
+          liste.insertOne({nom: newplayer.nom, kim: newplayer.kim, sid: socket.id});
+          // J'ajoute le joueur à la liste des scores
+          scores.insertOne({nom: newplayer.nom, score: newplayer.score, sid: socket.id});
+          // Je déclenche les globe de survie
+          setInterval(globesDeVie, 5000);
+
+          // Je crée un nouveau joueur de l'autre côté avec les données envoyées (dont mon nom)
+          const nom = newplayer.nom;
+          const kim = newplayer.kim;
+          const idImage = socket.id;
+          const myJoueur = new Joueur(nom, kim, idImage); // J'envoie son nom/type dans la fonction
+          listeDesJoueurs[socket.id] = myJoueur;
+          const clientObject = myJoueur.getClientObject();
+
+          socket.emit('myJoueurCreated', clientObject); // Crée mon joueur
+          socket.broadcast.emit('ajoutDunNouveauJoueur', clientObject); // Notification de ma création
+          socket.emit('jeRecupLaListeDesJoueurs', listeDesJoueurs) // Je récupère les anciens joueurs
+
+          const barre = new InterfaceJoueur();
+          socket.emit('creerLaBarreDeVie', barre);
+
+          const barreScore = new InterfaceScoreJoueur();
+          socket.emit('creerLaBarreDeScore', barreScore);
+
+          const barreScoreGeneral = new ScoreGeneral();
+          socket.emit('creerLaBarreDeScoreGeneral', barreScoreGeneral);
+
+          // Je récupère la liste des scores
+          scores.find().limit(10).sort({score: -1}).toArray(function(err, resultats) {
+            if (err) {
+              throw err;
+            } else {
+              // Envoyer les scores
+              socket.emit('creeLeTableau', resultats)
+              // socket.broadcast.emit('creeLeTableau', resultats)
+            }
+          })
+        }
+      })
+    })
+
+    // MAJ de la position des joueurs
+    socket.on('moveJoueur', function(coords) {
+      if (listeDesJoueurs[socket.id]) {
+        const myJoueur = listeDesJoueurs[socket.id];
+        myJoueur.x = coords.x;
+        myJoueur.y = coords.y;
+
+        // MAJ de la position
+        const clientObject = myJoueur.getClientObject();
+        socketIO.emit('updateJoueurPosition', clientObject);
+      }
+    });
+
+    // Si un joueur meurt, je vais chercher son socket.id et je le renvoie
+    socket.on('mortDunJoueur', function() {
+      console.log("Dead:" + socket.id);
+      var monIdPutain = socket.id;
+      socket.emit('jeVeuxMonID', monIdPutain)
+      socket.broadcast.emit('jeVeuxMonID', monIdPutain)
+    })
+
+    // Déconnexion d'un joueur
+    socket.on('disconnect', (reason) => {
+      // Supprime de la liste BDD le joueur
+      liste.deleteOne({sid: socket.id})
+      // S'il n'y a plus personne de connecté sur socket local, supprime la liste DB
+      if (size < 1) {
+        liste.drop();
+        console.log("Erreurs supprimées");
+      }
+
+      console.log('Déconnexion :', socket.id, reason);
+      if (listeDesJoueurs[socket.id] && listeDesJoueurs[socket.id].id) {
+        // Je le supprime du tableau
+        const joueurId = listeDesJoueurs[socket.id].id;
+        delete listeDesJoueurs[socket.id];
+        // Je le supprime de la bdd
+        socketIO.emit('removeJoueur', joueurId);
+      }
+    });
+
+    // Globe disparaît quand quelqu'un a cliqué dessus
+    socket.on('globeEnglouti', function(data) {
+      socket.emit('globeDisparait', data)
+      socket.broadcast.emit('globeDisparait', data)
+    })
+
+    // Augmente le score dans la base de données (grâce au socketId)
+    socket.on('scoreAugmente', function(data) {
+
+      // Je reçois le nouveau score, je cherche son socket.id et je l'enregistre
+      scores.updateOne({
+        sid: socket.id
+      }, {
+        $set: {
+          score: data.score
+        }
+      }, {
+        upsert: true // rechercher et rajouter
+      });
+
+      // Je renvoie le nouveau tableau
+      setTimeout(function() {
+        scores.find().limit(8).sort({score: -1}).toArray(function(err, resultats) {
+          if (err) {
+            throw err;
+          } else {
+            // Envoyer les scores
+            socket.emit('majTableau', resultats)
+            socket.broadcast.emit('majTableau', resultats)
+          }
+        })
+      }, 2000)
+
+    });
+
+  }); // Fin du socket connect
+
+  monServeurIo.listen(port, function() {
+    console.log('Listen on ' + port);
+  });
+
+}) // Fin du MongoClient
