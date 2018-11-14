066653f5a71912d8247cf12d89c01ce577714b5e
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2018-11-14 13:02:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const monServeurIo = require('http').createServer(monServeur);
 const socketIO = require('socket.io')(monServeurIo);
 
 const port = process.env.PORT || 3000;
-const url = 'mongodb+srv://guillaume:86327417Gd%2E@donaldvskim-zqkaf.mongodb.net'
+const url = 'mongodb+srv://guillaume:%2E@donaldvskim-zqkaf.mongodb.net'
 // const url = 'mongodb://localhost:27017'
 const dbName = 'users'
 
